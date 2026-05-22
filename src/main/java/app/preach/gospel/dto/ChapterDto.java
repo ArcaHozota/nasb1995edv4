9caf2f72a524ba52337cfb723a409a0ddacacc41
ArkamaHozota <arkamahozota@gmail.com>
2026-05-23 02:42:01 +0900

    1.28

--- src/main/java/app/preach/gospel/dto/ChapterDto.java
+++ src/main/java/app/preach/gospel/dto/ChapterDto.java
@@ -1,6 +1,7 @@
 package app.preach.gospel.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 
 /**
  * 章節情報転送クラス
@@ -9,25 +10,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
  * @since 1.00beta
  */
 @Schema(description = "章節情報転送クラス")
-public record ChapterDto(
-
-		/*
-		 * ID
-		 */
-		Integer id,
-
-		/*
-		 * 名称
-		 */
-		String name,
-
-		/*
-		 * 日本語名称
-		 */
-		String nameJp,
-
-		/*
-		 * 書別ID
-		 */
-		String bookId) {
+public record ChapterDto(@Schema(description = "ID", example = "15", requiredMode = RequiredMode.REQUIRED) Integer id,
+		@Schema(description = "英語名称", example = "Psalms 33", requiredMode = RequiredMode.REQUIRED) String name,
+		@Schema(description = "日本語名称", example = "詩編90編", requiredMode = RequiredMode.REQUIRED) String nameJp,
+		@Schema(description = "書別ID", example = "33", requiredMode = RequiredMode.REQUIRED) String bookId) {
 }
--- src/main/java/app/preach/gospel/dto/VerseDto.java
+++ src/main/java/app/preach/gospel/dto/VerseDto.java
@@ -1,6 +1,7 @@
 package app.preach.gospel.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 
 /**
  * 節別情報転送クラス
@@ -9,30 +10,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
  * @since 1.00beta
  */
 @Schema(description = "節別情報転送クラス")
-public record VerseDto(
-
-		/*
-		 * ID
-		 */
-		Long id,
-
-		/*
-		 * 名称
-		 */
-		String name,
-
-		/*
-		 * 内容
-		 */
-		String textEn,
-
-		/*
-		 * 日本語内容
-		 */
-		String textJp,
-
-		/*
-		 * 章節ID
-		 */
-		String chapterId) {
+public record VerseDto(@Schema(description = "ID", example = "33", requiredMode = RequiredMode.REQUIRED) Long id,
+		@Schema(description = "英語名称", example = "Luke 21:33", requiredMode = RequiredMode.REQUIRED) String name,
+		@Schema(description = "英語内容", example = "Heaven and earth will pass away, but My words will by no means pass away.", requiredMode = RequiredMode.REQUIRED) String textEn,
+		@Schema(description = "日本語内容", example = "天地は滅びるが、わたしの言葉は決して滅びない。", requiredMode = RequiredMode.REQUIRED) String textJp,
+		@Schema(description = "章節ID", example = "33", requiredMode = RequiredMode.REQUIRED) String chapterId) {
 }
