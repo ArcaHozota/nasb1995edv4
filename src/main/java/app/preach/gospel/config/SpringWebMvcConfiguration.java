a5bb6b591a01912bc88fbc7b5d496223daa78630
ArkamaHozota <arkamahozota@gmail.com>
2026-05-23 17:13:06 +0900

    1.48

--- src/main/java/app/preach/gospel/config/SpringWebMvcConfiguration.java
+++ src/main/java/app/preach/gospel/config/SpringWebMvcConfiguration.java
@@ -10,9 +10,8 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.lang.NonNull;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
@@ -28,18 +27,18 @@ import lombok.extern.log4j.Log4j2;
  */
 @Log4j2
 @Configuration
-public class SpringWebMvcConfiguration extends WebMvcConfigurationSupport {
+public class SpringWebMvcConfiguration implements WebMvcConfigurer {
 
-	/**
-	 * 静的なリソースのマッピングを設定する
-	 *
-	 * @param registry レジストリ
-	 */
-	@Override
-	protected void addResourceHandlers(final @NonNull ResourceHandlerRegistry registry) {
-		log.info(ProjectConstants.MESSAGE_SPRING_MAPPER);
-		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-	}
+//	/**
+//	 * 静的なリソースのマッピングを設定する
+//	 *
+//	 * @param registry レジストリ
+//	 */
+//	@Override
+//	protected void addResourceHandlers(final @NonNull ResourceHandlerRegistry registry) {
+//		log.info(ProjectConstants.MESSAGE_SPRING_MAPPER);
+//		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+//	}
 
 	/**
 	 * ビューのコントローラを定義する
@@ -47,7 +46,7 @@ public class SpringWebMvcConfiguration extends WebMvcConfigurationSupport {
 	 * @param registry
 	 */
 	@Override
-	protected void addViewControllers(final @NonNull ViewControllerRegistry registry) {
+	public void addViewControllers(final @NonNull ViewControllerRegistry registry) {
 		registry.addViewController("/home/to-login").setViewName("login-toroku");
 //		registry.addViewController("/home/to-mainmenu").setViewName("mainmenu");
 		registry.addViewController("/hymns/to-random-five").setViewName("hymns-random-five");
@@ -59,7 +58,7 @@ public class SpringWebMvcConfiguration extends WebMvcConfigurationSupport {
 	 * @param converters コンバーター
 	 */
 	@Override
-	protected void extendMessageConverters(final @NonNull List<HttpMessageConverter<?>> converters) {
+	public void extendMessageConverters(final @NonNull List<HttpMessageConverter<?>> converters) {
 		log.info(ProjectConstants.MESSAGE_SPRING_MVCCONVERTOR);
 		final var messageConverter = new MappingJackson2HttpMessageConverter();
 		messageConverter.setObjectMapper(new JacksonObjectMapper());
