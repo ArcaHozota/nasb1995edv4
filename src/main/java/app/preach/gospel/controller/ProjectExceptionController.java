d99277883e4b0363503d483af7a7a042ab12122f
ArkamaHozota <arkamahozota@gmail.com>
2026-05-26 19:24:22 +0900

    1.62

---
+++ src/main/java/app/preach/gospel/controller/ProjectExceptionController.java
@@ -0,0 +1,56 @@
+package app.preach.gospel.controller;
+
+import org.jooq.exception.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import app.preach.gospel.common.ProjectConstants;
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * SpringMVC例外処理インターセプト
+ *
+ * @author ArkamaHozota
+ * @since 2.00
+ */
+@Log4j2
+@ControllerAdvice
+public class ProjectExceptionController {
+
+//	@ExceptionHandler(AccessDeniedException.class)
+//	public Object handleAccessDeniedException(final AccessDeniedException exception) {
+//		log.error("Access denied: {}", exception.getMessage());
+//		return ResponseEntity.status(HttpStatus.FORBIDDEN).contentType(MediaType.APPLICATION_JSON)
+//				.body(ProjectConstants.MESSAGE_SPRINGSECURITY_REQUIRED_AUTH);
+//	}
+
+	@ExceptionHandler(DataAccessException.class)
+	@ResponseBody
+	public Object handleDataAccessException(final DataAccessException exception) {
+		final var errorMessage = exception.getMessage() != null ? exception.getMessage()
+				: ProjectConstants.MESSAGE_STRING_FATAL_ERROR;
+		log.error("Database error: {}", errorMessage);
+		return ResponseEntity.status(HttpStatus.CONFLICT).contentType(MediaType.APPLICATION_JSON).body(errorMessage);
+	}
+
+	@ExceptionHandler(Exception.class)
+	public String handleException(final Exception exception, final Model model) {
+		log.error("処理中にエラーが発生しました：", exception);
+		if (exception.getMessage() != null) {
+			model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR.toString());
+			model.addAttribute("message", exception.getMessage());
+			model.addAttribute(ProjectConstants.ATTRNAME_EXCEPTION, exception);
+			return "error";
+		}
+		model.addAttribute("status", HttpStatus.CONFLICT.toString());
+		model.addAttribute("message", ProjectConstants.MESSAGE_STRING_FATAL_ERROR);
+		model.addAttribute(ProjectConstants.ATTRNAME_EXCEPTION, exception);
+		return "error";
+	}
+
+}
