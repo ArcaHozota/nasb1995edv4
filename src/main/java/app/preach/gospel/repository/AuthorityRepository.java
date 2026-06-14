6c42792dc740d721abb31f7858b99ab7ae9b15b9
ArkamaHozota <arkamahozota@gmail.com>
2026-06-15 00:57:44 +0900

    4.03

--- src/main/java/app/preach/gospel/repository/AuthorityRepository.java
+++ src/main/java/app/preach/gospel/repository/AuthorityRepository.java
@@ -2,7 +2,6 @@ package app.preach.gospel.repository;
 
 import java.util.List;
 
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -16,11 +15,11 @@ import app.preach.gospel.model.Authority;
 @Repository
 public interface AuthorityRepository extends ListCrudRepository<Authority, Long> {
 
-	// 全権限をID昇順で取得
-	// 対応Dao: AuthorityDao#selectAll
-	List<Authority> findAllByOrderByIdAsc();
-
 	// IDリストで権限を取得
 	// 対応Dao: AuthorityDao#selectByIds (foreachはSpring Data命名規則で代替)
 	List<Authority> findAllByIdIn(List<Long> ids);
+
+	// 全権限をID昇順で取得
+	// 対応Dao: AuthorityDao#selectAll
+	List<Authority> findAllByOrderByIdAsc();
 }
